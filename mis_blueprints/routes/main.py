821d4e13495daa135023a41d3c3463659f68b3c8
yenary <almeidayenary@gmail.com>
2026-09-05 22:46:09 -0500

    cambie tamaño y disposicion de tarjetas de proyectos e instructores

--- mis_blueprints/routes/main.py
+++ mis_blueprints/routes/main.py
@@ -9,7 +9,7 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def inicio():
-    query1 = "SELECT id_proyectos AS id, nombre, descripcion_corta AS descripcion, foto_principal AS foto FROM proyectos limit 2"
+    query1 = "SELECT id_proyectos AS id, nombre, descripcion_corta AS descripcion, foto_principal AS foto FROM proyectos limit 3"
     query2 = "SELECT id_colegios AS id, nombre, slogan, logo FROM colegios"
     query3 = "SELECT id_municipios AS id, nombre, foto FROM municipios"
     query4 = "SELECT i.id_instructor AS id, i.nombres, i.apellidos, p.nombre_profesion as profesion, i.foto FROM instructor i INNER JOIN profesiones p ON i.id_profesion=p.id_profesion"
